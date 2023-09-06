167c7c7a8a5bf79787bae92f5c4251a245eb210b
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-09-06 10:53:24 -0400

    eeg

--- SIdyads.m
+++ SIdyads.m
@@ -1,62 +1,64 @@
-function total_accuracy = SIdyads(subj_number, run_number, win, dispSize, break_frequency, iti_length, send_trig)
-% Presents the social interactions dyads experiment for ECoG
-%
+function run_number = SIdyads(subjName, run_number, with_Eyelink, send_trigger)
+% Presents the social interaction localizer task
 % Inputs:
-% subj_number - an integer identifying the subject
-% run_number - an integer identifying the current run
-% win - the window pointer for psychtoolbox
-% dispSize - the rectangle where the stimulus should be presented
-% break_frequency - an integer indicating how often to have breaks in the
-% There are 275 videos to a run. A value of 2, for example, would lead to a
-% break every ~138 videos (275/2).
-% iti_length - the amount of time between stimuli in seconds
-% send_trig - boolean indicating whether to check for responses
-%
-% Outputs:
-% The total accuracy for the current run
-%
-% Written by Emalie McMahon Oct 3, 2022
+% subjName as an integer used when saving and loading the sequences
+% with_Eyelink is a binaray value whether the code should connect to and
+% send messages to the eyetracker
+% send_trigger is a binaray value whether the code should send a trigger to
+% the EEG system
+% runNum is an integer used when saving and loading the sequences
+% Written by Emalie McMahon Sept 6, 2023
 
-
-
+debug = 0; % Set to zero unless no argumets are passed, then enter debug mode
 if nargin < 1
-    subj_number = 77;
+    subjName = 77;
     run_number = 1;
-    send_trig = 0;
-    iti_length = 1;
-    break_frequency = 0; 
-    mac = 1;
-    
-    %% open window
-    commandwindow;
-    %     HideCursor;
-    Screen('Preference', 'SkipSyncTests', 2); %1 for mac %2 for window
-
-    % Uncomment for debugging with transparent screen
-%     AssertOpenGL;
-%     PsychDebugWindowConfiguration;
-
-    screen = max(Screen('Screens'));
-    [win, rect] = Screen('OpenWindow', screen, 0);
-    [x0, y0] = RectCenter(rect);
-    Screen('Blendfunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    dispSize = [x0-500 y0-500 x0+500 y0+500];
-
-
-    priorityLevel=MaxPriority(win);
-    Priority(priorityLevel);
+    with_Eyelink = 0;
+    send_trigger = 0; 
+    debug = 1;
 end
 
 % make output directories
 curr = pwd;
-topout = fullfile(curr, 'data', ['sub',sprintf('%02d', subj_number)]);
+topout = fullfile(curr, 'data', ['subj',sprintf('%03d', subjName)]);
 matout = fullfile(topout, 'matfiles');
-if ~exist(matout, 'dir'); mkdir(matout); end 
 timingout = fullfile(topout, 'timingfiles');
-if ~exist(timingout, 'dir'); mkdir(timingout); end 
+runfiles = fullfile(topout,'runfiles');
+edfout = fullfile(topout, 'edfs');
+if ~exist(matout); mkdir(matout); end
+if ~exist(timingout); mkdir(timingout); end
+if ~exist(edfout); mkdir(edfout); end
 
-s=sprintf('Subject number is %g. Run number is %g. ', subj_number, run_number);
+if ~exist(topout, 'dir')
+    s=sprintf('Run files do not exist of subject %g. Make run files before continuing.', subjName);
+    error(s);
+end
+
+
+if isempty(run_number)
+    % If the run_number is not assigned, find the last run and
+    % increment by 1.
+    files = dir(fullfile(timingout, '*.csv'));
+    if ~isempty(files)
+        runs = [];
+        for i=1:length(files)
+            f = strsplit(files(i).name, '_');
+            f = strsplit(f{1},'n');
+            runs(i) = str2num(f{end});
+        end
+        run_number = max(runs) + 1;
+    else
+        run_number = 1;
+    end
+end
+
+s=sprintf('Subject number is %g. Run number is %g. ', subjName, run_number);
 fprintf('\n%s\n\n ',WrapString(s));
+
+
+%% load video list
+ftoread = dir(fullfile(runfiles,['run',sprintf('%03d', run_number),'*.csv']));
+T = readtable(fullfile(ftoread.folder,ftoread.name));
 
 %% Experiment variables
 curr_date = datestr(datetime('now'), 'yyyymmddTHHMMSS');
@@ -66,76 +68,148 @@ rate = 1;
 sound = 0;
 blocking = 1;
 stimulus_length = 0.5;
-iti_jitter = 0.05;
-n_response = 25;
-n_real = 250;
-n_extra_iti = 25;
-ending_wait_time = 0;
-start_wait_time = iti_length;
+TR = .75;
+iti_length = TR;
+ending_wait_time = 1;
+start_wait_time = TR;
 n_frames = 15;
+if debug
+    n_trials = 5;
+else
+    n_trials = height(T);
+end
 
+expected_duration = (height(T) * (stimulus_length + iti_length)) + ending_wait_time + start_wait_time;
+fprintf('Expected duration: %g min \n\n', expected_duration / 60);
+sca;
+
+
+%% Make stimulus presentation table
+%initialize data columns
+T.onset_time = zeros(height(T),1);
+T.offset_time = zeros(height(T),1);
+T.duration = zeros(height(T),1);
+T.response = zeros(height(T),1);
+
+%Get the name of the first movie
+for itrial = 1:height(T)
+    video_name = T.video_name{itrial};
+    if T.condition(itrial) == 1
+        T.movie_path{itrial} = fullfile(curr, 'videos','dyad_videos_3000ms',video_name);
+    elseif T.condition(itrial) == 0
+        T.movie_path{itrial} = fullfile(curr, 'videos','crowd_videos_3000ms',video_name);
+    end
+end
+
+movie = zeros(n_trials, 1);
+
+%% open window
+commandwindow;
+%     HideCursor;
+Screen('Preference','SkipSyncTests',1);
+
+% Uncomment for debugging with transparent screen
+% AssertOpenGL;
+% PsychDebugWindowConfiguration;
+
+%Suppress frogs
+Screen('Preference','VisualDebugLevel', 0);
+
+screen = max(Screen('Screens'));
+[win, rect] = Screen('OpenWindow', screen, 0);
+[x0,y0] = RectCenter(rect);
+Screen('Blendfunction', win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+dispSize = [x0-500 y0-500 x0+500 y0+500];
+
+priorityLevel=MaxPriority(win);
+Priority(priorityLevel);
+
+% Photodiode variables
 photodiode_square = [95 y0-12 119 y0+12];
 black = BlackIndex(window);
 white = WhiteIndex(window);
 
+%% Init eyelink
 
-%% Make stimulus presentation table
-
-%load video list
-video_names = dir('social_dyad_videos_500ms/short_videos/*.mp4');
-vid_inds = randperm(length(video_names));
-vid_inds = vid_inds(1:n_real); 
-video_list = cell(n_real, 1);
-for i = 1:n_real
-    video_list{i} = video_names(vid_inds(i)).name;
-end
-
-%get filler videos
-crowd_names = dir(fullfile('social_dyad_videos_500ms','crowd_videos_short','*.mp4'));
-inds = randperm(length(crowd_names));
-inds = inds(1:n_response); 
-response_videos = cell(n_response,1);
-for i = 1:n_response
-    response_videos{i} = crowd_names(inds(i)).name;
-end
-
-video_list = [video_list, num2cell(ones(n_real, 1)), num2cell(zeros(n_real, 1)), num2cell(zeros(n_real, 1)), num2cell(zeros(n_real, 1)), num2cell(zeros(n_real, 1)), num2cell(zeros(n_real, 1));...
-    response_videos, num2cell(zeros(n_response, 1)), num2cell(zeros(n_response, 1)), num2cell(zeros(n_response, 1)), num2cell(zeros(n_response, 1)), num2cell(zeros(n_response, 1)), num2cell(zeros(n_response, 1))];
-video_table = cell2table(video_list);
-video_table.Properties.VariableNames = {'video_name' 'condition' 'onset_time' 'offset_time' 'duration' 'response' 'response_time'};
-T = video_table(randperm(size(video_table,1)), :);
-
-add_jitter = [ones(n_extra_iti,1); zeros(size(T,1)-n_extra_iti-1,1)];
-add_jitter = add_jitter(randperm(length(add_jitter)));
-add_jitter(end+1) = ending_wait_time/iti_jitter;
-T.added_jitter = add_jitter;
-n_trials = size(T, 1);
-
-%Save the presentation table at the beginning just in case
-filename = fullfile(timingout,['run', sprintf('%03d', run_number), '_',curr_date,'.csv']);
-writetable(T, filename);
-
-%Get the name of the first movie
-for itrial = 1:n_trials
-    video_name = split(T.video_name{itrial},'.');
-    video_name = [video_name{1},'.mp4'];
-    if T.condition(itrial) == 1
-        T.movie_path{itrial} = fullfile(curr, 'social_dyad_videos_500ms','short_videos',video_name);
-    elseif T.condition(itrial) == 0
-        T.movie_path{itrial} = fullfile(curr, 'social_dyad_videos_500ms','crowd_videos_short',video_name);
+if with_Eyelink
+    if EyelinkInit()~= 1
+        return;
     end
+    
+    [~,vs] = Eyelink('GetTrackerVersion');
+    fprintf('Running experiment on a ''%s'' tracker.\n', vs );
+    
+    edfFile=['run', sprintf('%03d', run_number)]; %fullfile(edfout,['run',  sprintf('%03d', run_number)]);
+    el=EyelinkInitDefaults(win);%window is the window you have opened with screen function
+    % open file to record data to
+    Eyelink('Openfile', edfFile);
+    
+    % this line will perform the calibration
+    Eyelink('command', 'calibration_type = HV9');
+    EyelinkDoTrackerSetup(el);
+    
+    % set up configurations.
+    Eyelink('command', 'recording_parse_type = GAZE');
+    Eyelink('command', 'saccade_acceleration_threshold = 8000');
+    Eyelink('command', 'saccade_velocity_threshold = 30');
+    Eyelink('command', 'saccade_motion_threshold = 0.15');
+    Eyelink('command', 'saccade_pursuit_fixup = 60');
+    
+    %	set EDF file contents
+    Eyelink('command', 'file_event_filter = LEFT,RIGHT,FIXATION,SACCADE,BLINK,MESSAGE,BUTTON');
+    Eyelink('command', 'file_sample_data  = LEFT,RIGHT,GAZE,GAZERES,HREF,AREA');
+    Eyelink('command', 'file_event_data  = GAZE,GAZERES,AREA,VELOCITY,HREF');
+    
+    %	set link data (used for gaze cursor)
+    Eyelink('command', 'link_event_filter = LEFT,RIGHT,FIXATION,SACCADE,BLINK,MESSAGE,BUTTON');
+    Eyelink('command', 'link_sample_data  = LEFT,RIGHT,GAZE,GAZERES,HREF,AREA');
+    Eyelink('command', 'link_event_data  = GAZE,GAZERES,AREA,VELOCITY,HREF');
+    Eyelink('StartRecording');
+    % record a few samples before we actually start displaying
+    WaitSecs(0.1);
+    
+    %     eyeLinkCheck(win,x0,y0); %Additional Eyelink Validation that saves the
+    %position at the beginning of the block data to help diagnose systematic
+    %errors.
 end
-movie = zeros(n_trials, 1);
-still_loading = 1; %The first movie is not yet loaded
 
-%% Experiment loop
+%% Init EEG
+%triggers: 1 = movie start; 2 = movie end; 3 = response
+if send_trigger
+    %    create an instance of the io64 object
+    ioObj = io64; %#ok<*UNRCH>
+    %   initialize the interface to the inpoutx64 system driver
+    status = io64(ioObj); % if status = 0, you are now ready to write and read to a hardware port
+    %  EEG port address
+    address = hex2dec('4FB8');%standard LPT1 output port address
+end
 
-% experiment start time
-start = GetSecs();
-Screen('FillRect', win, white, squarephoto); % to time it on the photodiode
+%% Task instructions and start with the trigger
+instructions='Watch the people in each video. If there are more than 2 people, press any button. Press any button to begin.';
+DrawFormattedText2(instructions,'win',win,'sx','center','sy','center','xalign','center','yalign', 'center','baseColor',[255, 255, 255]);
 Screen('Flip', win);
 
-% wait half a second to start
+%% WAIT FOR TRIGGER TO START
+still_loading = 1;
+while 1
+    if KbCheck
+        break;
+    end
+    
+    if still_loading
+        movie(1) = Screen('OpenMovie', win, T.movie_path{1}, async, preloadsecs);
+        if movie(1) > 0; still_loading = 0; end
+    end
+end
+
+
+%% Experiment loop
+% experiment start time
+% try
+start = GetSecs();
+Screen('Flip', win);
+
+% wait 2 TRs to start
 while (GetSecs-start<start_wait_time)
     if still_loading
         movie(1) = Screen('OpenMovie', win, T.movie_path{1}, async, preloadsecs);
@@ -143,99 +217,149 @@ while (GetSecs-start<start_wait_time)
     end
 end
 
-if send_trig, io64(ioObj,address,0); end %set trigger to 0 
+if send_trig, io64(ioObj,address,0); end %set trigger to 0
 for itrial = 1:n_trials
-    still_loading = 1; %Value of one means that the next movie is still loading
-    keyPressed = 0; %Whether a key has been pressed
-    
+    still_loading = 1;
+    response = 0;
+    trial_start = GetSecs;
     Screen('SetMovieTimeIndex', movie(itrial), 0);
     Screen('PlayMovie', movie(itrial), rate, 1, sound);
     frame_stamps = zeros(n_frames,1);
-    for idx = 1:n_frames
+    trial_end = trial_start + stimulus_length;
+    iti_end = trial_end + iti_length;
+    T.onset_time(itrial) = trial_start - start;
+    
+    if with_Eyelink %inside the trial function
+        % these messages will be recorded in the output file determining the begining of the trial
+        Eyelink('Message', ['TRIALID ', num2str(itrial)]);
+        Eyelink('Message', ['TRIAL_VAR_DATA ', T.video_name{itrial}]);
+        Eyelink('Message', 'STIMULUS_START');
+    end
+    
+    frame_counter = 1;
+    while 1
+        if frame_counter == (n_frames+1) || GetSecs > (trial_end-(1/60))
+            break;
+        end
+        
         tex = Screen('GetMovieImage', win, movie(itrial), blocking);
         Screen('DrawTexture', win, tex, [], dispSize);
-        Screen('FillRect', win, black, squarephoto); % to time it on the photodiode
-
+        Screen('FillRect', win, black, photodiode_square); % to time it on the photodiode
         %%%% BEGIN SACRED TIMING SENSITIVE SECTION%%%%
         [frame_stamps(idx),~,~,~] = Screen('Flip',win); % Odd sequencing, but want 'ScreenFlip' right after DAQ on
         if idx==1 && send_trig
             io64(ioObj,address,1);  % DAQ On
             WaitSecs(0.005);        % Wait 5 ms
         end
-        Screen('Close',tex);
+        Screen('Close', tex);
         
-        if ~keyPressed && ~mac; [keyPressed, keyTime, ~] = KbCheck; end %Check if a key has been pressed
+        if ~response
+            if KbCheck
+                response = 1;
+                T.response(itrial) = 1;
+                
+                if send_trig, io64(ioObj,address,3); WaitSecs(0.005); end %send trigger for response
+            end
+        end
+        frame_counter = frame_counter + 1;
     end
-    Screen('FillRect', win, white, squarephoto); % to time it on the photodiode
+    Screen('FillRect', win, white, photodiode_square); % to time it on the photodiode
     real_trial_end = Screen('Flip', win);
     if send_trig
         io64(ioObj,address,2);  % DAQ On
         WaitSecs(0.005);        % Wait 5 ms
-    end 
+    end
     %%%% END SACRED TIMING SENSITIVE SECTION%%%%
-    T.onset_time(itrial) = frame_stamps(1) - start;
     
-    %Save end time and close movie
+    %Get end time and close movie
     T.offset_time(itrial) = real_trial_end - start;
-    T.duration(itrial) = real_trial_end - start - T.onset_time(itrial);
+    T.duration(itrial) = real_trial_end - trial_start;
     Screen('CloseMovie', movie(itrial));
     
-    while still_loading && itrial < n_trials
-        movie(itrial+1) = Screen('OpenMovie', win, T.movie_path{itrial+1}, async, preloadsecs);
-        if movie(itrial+1) > 0; still_loading = 0; end
+    message_sent = 0;
+    while (GetSecs<iti_end)
+        if with_Eyelink && ~message_sent
+            Eyelink('Message','STIMULUS_OFF');
+            message_sent = 1;
+        end
+        if still_loading && itrial ~= n_trials
+            movie(itrial+1) = Screen('OpenMovie', win, T.movie_path{itrial+1}, async, preloadsecs);
+            if movie(itrial+1) > 0; still_loading = 0; end
+        end
+        
+        if ~response
+            if KbCheck
+                response = 1;
+                T.response(itrial) = 1;
+                
+                if send_trig, io64(ioObj,address,3); WaitSecs(0.005); end %send trigger for response
+                
+            end
+        end
     end
     
-    iti_end = T.onset_time(itrial) + stimulus_length + iti_length + T.added_jitter(itrial)*iti_jitter;
-    %Wait until the end of the ITI
-    while ((GetSecs-start)<iti_end)
-        if ~keyPressed && ~mac; [keyPressed, keyTime, ~] = KbCheck; end %Check if a key has been pressed
-    end
-    
-    if keyPressed
-        T.response_time(itrial) = keyTime - T.onset_time(itrial);
-        T.response(itrial) = 1;
-    end 
-    
-    
-    %% Experiment break
-    if break_frequency 
-        if rem(itrial, round(n_trials/break_frequency)) == 0
-            current_accuracy = round(mean(T.condition(1:itrial) ~= T.response(1:itrial)) * 100); 
-            break_text=sprintf('Your accuracy for this section is %g%%. \n Press any button to continue.', current_accuracy);
-            DrawFormattedText2(break_text,'win',win,'sx','center','sy','center','xalign','center','yalign', 'center','baseColor',[255, 255, 255]);
+    if itrial ~= height(T)
+        if T.block(itrial) ~= T.block(itrial + 1)
+            DrawFormattedText2('Take a short break./n Press any button when ready to continue.','win',win,'sx','center','sy','center','xalign','center','yalign', 'center','baseColor',[255, 255, 255]);
             Screen('Flip', win);
-
-            %If the button box is connected, it should wait for a button press to continue
-            if ~mac
-                key = 0;
-                while (~key)
-                    [keyPressed, ~, keyCode] = KbCheck;
-                    if (keyPressed)
-                        key = find (keyCode) == KbName ('f');
-                    end
+            fprintf('Break in experiment');
+            while 1
+                if KbCheck
+                    break;
                 end
-            else
-                WaitSecs(2.5);
-            end 
-
-            save(fullfile(matout,['run', sprintf('%03d', run_number) '_',curr_date,'.mat']))
-        end 
-    end 
+            end
+        end
+    else %itrial == height(T)
+        instructions = 'You may sit back.\nLonger break is beginning.\nThis window will close.\n';
+        
+        DrawFormattedText2(instructions,'win',win,'sx','center','sy','center','xalign','center','yalign', 'center','baseColor',[255, 255, 255]);
+        Screen('Flip', win);
+        fprintf('End of run');
+        WaitSecs(3);
+    end
 end
 
+%% Save data
+actual_duration = GetSecs() - start;
 save(fullfile(matout,['run', sprintf('%03d', run_number) '_',curr_date,'.mat']))
+filename = fullfile(timingout,['run', sprintf('%03d', run_number), '_',curr_date,'.csv']);
 writetable(T, filename);
+ShowCursor;
+Screen('CloseAll')
 
-%Print participant performance
+%% save eyelink and close
+if with_Eyelink
+    Eyelink('Stoprecording')
+    Eyelink('CloseFile');
+    try
+        fprintf('Receiving data file ''%s''\n', edfFile );
+        status=Eyelink('ReceiveFile',edfFile);
+        if status > 0
+            fprintf('ReceiveFile status %d\n', status);
+        end
+        if exist(edfFile, 'file')
+            fprintf('Data file ''%s'' can be found in ''%s''\n', edfFile, pwd );
+        end
+    catch
+        fprintf('Problem receiving data file ''%s''\n', edfFile );
+    end
+    Eyelink('ShutDown');
+    
+    try
+        movefile([edfFile,'.edf'], fullfile(edfout, [edfFile, '_', curr_date, '.edf']));
+        move_error = 'No';
+    catch
+        move_error = 'Yes';
+    end
+    fprintf(['Move EDF Error? ',move_error,'\n'])
+end
+
+%% Print participant performance
 false_alarms = sum(T.response(T.condition == 1) == 1);
 hits = sum(T.response(T.condition == 0) == 1);
-total_accuracy = round(mean(T.condition ~= T.response) * 100);
-s=sprintf('%g hits out of %g crowd videos. %g false alarms out of %g dyad videos. Overall accuracy is %g%%.', hits, n_response, false_alarms, n_real, total_accuracy);
+total_accuracy = mean(T.condition ~= T.response);
+s=sprintf('%d hits \n%d false alarms\nOverall accuracy was %0.2f.', hits, false_alarms, total_accuracy);
 fprintf('\n\n\n%s\n',WrapString(s));
-s=sprintf('Subject %g, run %g: Complete. ', subj_number, run_number);
-fprintf('\n%s\n ',WrapString(s));
+s=sprintf('Experiment duration %0.1f s', actual_duration);
+fprintf('\n\n\n%s\n',WrapString(s));
 
-if nargin < 1
-    sca;
-end 
-end
