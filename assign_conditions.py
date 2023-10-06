037cf10d345b5c283cb5f1c9b64af611132f52f0
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-10-06 11:27:28 -0400

    updating conditions

--- assign_conditions.py
+++ assign_conditions.py
@@ -8,10 +8,14 @@ import pandas as pd
 from glob import glob
 
 
-def load_video_names(file):
-    df = pd.read_csv(file)
-    df['condition'] = 1
-    return df
+def load_video_names(files, n_blocks):
+    df = []
+    for f in files:
+        cur = pd.read_csv(f)
+        df.append(cur)
+    vid_array = pd.concat(df).video_name.to_numpy()
+    np.random.shuffle(vid_array)
+    return vid_array.reshape((n_blocks, -1))
 
 
 def mk_output_paths(SID, toppath):
@@ -21,47 +25,37 @@ def mk_output_paths(SID, toppath):
 'runfiles')).mkdir(parents=True, exist_ok=True)
 
 
-def shuffle_trials(trials, n_blocks):
-    trials_shuffle = trials.copy()
-    seed = int((time.time() % 1) * 10000)
-    np.random.seed(seed)
-    np.random.shuffle(trials_shuffle)
-    return trials_shuffle.reshape((n_blocks, -1))
+def add_filler_trials(n=5):
+    crowd_videos = np.array(glob(os.path.join(os.getcwd(), 'videos', 
+                                              'crowd_videos_500ms', '*.mp4')))
+    movie_path = np.random.choice(crowd_videos, size=n, replace=False)
+    video_name = [Path(vid).stem + '.mp4' for vid in movie_path]
+    return pd.DataFrame({'video_name': video_name, 'condition': [0 for i in range(n)]})
+
+
+def mk_block(dyad_block, n):
+    crowd_df = add_filler_trials(int(len(dyad_block)*.1))
+    dyad_block_df = pd.DataFrame({'video_name': dyad_block,
+                                   'condition': np.ones(len(dyad_block), dtype='int')})
+    out_df = pd.concat([dyad_block_df, crowd_df]).sample(frac = 1)
+    out_df['block'] = n
+    return out_df
 
 
 def save_data(df, filename):
     df.to_csv(filename, index=False, header=True)
 
 
-def add_filler_trials(n=5):
-    crowd_videos = np.array(glob(os.path.join(os.getcwd(), 'videos', 
-'crowd_videos_500ms', '*.mp4')))
-    movie_path = np.random.choice(crowd_videos, size=n, replace=False)
-    video_name = [Path(vid).stem + '.mp4' for vid in movie_path]
-    return pd.DataFrame({'video_name': video_name, 'condition': [0 for i in range(n)]})
-
-
-def mk_block(videos, n):
-    crowd_df = add_filler_trials(int(len(videos)*.1))
-    block_df = pd.concat([videos, crowd_df])
-    shuffled_block = block_df.sample(frac = 1)
-    shuffled_block['block'] = n
-    return shuffled_block
-
-
-def get_past_runs(SID=77, n_runs=6, n_repeats=4, set='test'):
+def mk_condition_files(SID=77, n_runs=10, n_blocks=5):
     toppath = os.path.join('data', f'subj{str(SID).zfill(3)}')
     mk_output_paths(SID, toppath)
     print('\nWriting run files...')
-    videos = load_video_names('test.csv')
+    videos = load_video_names(['train.csv', 'test.csv'], n_blocks)
     for i in range(n_runs):
         run_df = []
-        print(f'run {i+1}')
-        for j in range(n_repeats):
-            print(f'block {j+1}')
-            run_df.append(mk_block(videos, j+1))
-            print(len(run_df))
-        print('\n')
+        for j in range(n_blocks):
+            block_df = mk_block(videos[j], j+1)
+            run_df.append(block_df)
         run_df = pd.concat(run_df)
         outname = os.path.join(toppath, 'runfiles', f'run{str(i+1).zfill(3)}.csv')
         save_data(run_df, outname)
@@ -71,13 +65,12 @@ def get_past_runs(SID=77, n_runs=6, n_repeats=4, set='test'):
 def getArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--sid', '-s', type=int, default=77)
-    parser.add_argument('--n_runs', type=int, default=3)
-    parser.add_argument('--n_repeats', type=int, default=4)
-    parser.add_argument('--set', type=str, default='test')
+    parser.add_argument('--n_runs', type=int, default=10)
+    parser.add_argument('--n_blocks', type=int, default=5)
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = getArgs()
-    get_past_runs(args.sid, args.n_runs, args.n_repeats)
+    mk_condition_files(args.sid, args.n_runs, args.n_blocks)
