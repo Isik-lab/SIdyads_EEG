b870caa17cc5d9287b723ba46b63b5d60bdf2a28
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-10-23 16:31:40 -0400

    test set

--- assign_conditions.py
+++ assign_conditions.py
@@ -8,14 +8,19 @@ import pandas as pd
 from glob import glob
 
 
-def load_video_names(files, n_blocks):
+def load_video_names(files, n_blocks, n_repeats):
     df = []
     for f in files:
         cur = pd.read_csv(f)
         df.append(cur)
     vid_array = pd.concat(df).video_name.to_numpy()
-    np.random.shuffle(vid_array)
-    return vid_array.reshape((n_blocks, -1))
+    out_array = []
+    for i in range(n_repeats):
+        cur_array = vid_array.copy()
+        np.random.shuffle(cur_array)
+        out_array.append(cur_array)
+    out_array = np.concatenate(out_array)
+    return out_array.reshape((n_blocks, -1))
 
 
 def mk_output_paths(SID, toppath):
@@ -55,11 +60,16 @@ def sample_normal_within_range(mu, sigma, low, high, size, precision=1):
     return np.array(samples)
 
 
-def mk_condition_files(SID=77, n_runs=8, n_blocks=5):
+def mk_condition_files(SID=77, n_runs=8, n_blocks=5, n_repeats=1, condition='both'):
     toppath = os.path.join('data', f'subj{str(SID).zfill(3)}')
     mk_output_paths(SID, toppath)
+
     print('\nWriting run files...')
-    videos = load_video_names(['train.csv', 'test.csv'], n_blocks)
+    if condition == 'both':
+        videos = load_video_names(['train.csv', 'test.csv'], n_blocks, n_repeats)
+    else:
+        videos = load_video_names([f'{condition}.csv'], n_blocks, n_repeats)
+
     for i in range(n_runs):
         run_df = []
         for j in range(n_blocks):
@@ -69,6 +79,7 @@ def mk_condition_files(SID=77, n_runs=8, n_blocks=5):
         run_df['iti'] = sample_normal_within_range(1.25, 0.1, 1, 1.5, len(run_df))
         outname = os.path.join(toppath, 'runfiles', f'run{str(i+1).zfill(3)}.csv')
         save_data(run_df, outname)
+        print(f'run {i+1} duration = {run_df.iti.sum() + (len(run_df) * .5)} s')
     print('\nRuns assigned. Closing...')
 
 
@@ -77,10 +88,12 @@ def getArgs():
     parser.add_argument('--sid', '-s', type=int, default=77)
     parser.add_argument('--n_runs', type=int, default=8)
     parser.add_argument('--n_blocks', type=int, default=5)
+    parser.add_argument('--n_repeats', type=int, default=8)
+    parser.add_argument('--condition', type=str, default='both')
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
     args = getArgs()
-    mk_condition_files(args.sid, args.n_runs, args.n_blocks)
+    mk_condition_files(args.sid, args.n_runs, args.n_blocks, args.n_repeats, args.condition)
